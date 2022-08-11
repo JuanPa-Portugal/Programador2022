47970a2faaa714a9b1918dd68d6ce5a198205db5
Ramisandivares <107085165+Ramisandivares@users.noreply.github.com>
2022-08-11 19:44:35 -0300

    SUBIENDO EJERCICIO N° DE PHYTON

--- ProyectoIntegradorEjerciciosPython/ejerciccio_1.py
+++ ProyectoIntegradorEjerciciosPython/ejerciccio_1.py
@@ -5,3 +5,11 @@
 
   Dichos datos se corresponden  con los datos del dueño de un perro (DNI, nombre, apellido, teléfono y dirección). Muestre en pantalla el teléfono del dueño si el DNI es mayor a 26000000.
 """
+EJERCICIO REALIZADO
+
+Dueno=[28957346, "Juan","Perez",4789689,"Belgrano 101"]
+
+if Dueno [0] > 26000000:
+
+   print ( "El telefono del dueño con el DNI", Dueno [ 0 ], "es", Dueno [3])
+
