0fb02da6de5d7a814b7d11f108c981a287f389b9
Mauro Martinez <105892844+Mauro-Martinez@users.noreply.github.com>
2022-08-11 23:08:33 -0300

    Update ejercicio_4.py
    RESOLVIENDO ACTIVIDAD

--- ProyectoIntegradorEjerciciosPython/ejercicio_4.py
+++ ProyectoIntegradorEjerciciosPython/ejercicio_4.py
@@ -5,3 +5,12 @@
 
 Recorrer la lista “Perro” con un bucle y mostrar sus elementos por pantalla , comenzando desde el último hasta el primero.
 """
+EJERCICIO RESULETO
+"""Creando mi lista Perro2 del tipo heterogénea"""
+
+Perro2 = [14, "Fido",  "12/12/2012", "Macho", 23546987]
+
+"""Recorriendo mi lista y mostrando sus elementos desde el ultimo al primero"""
+for x in range(len(Perro2)-1,-1,-1):
+  print(Perro2[x])
+
