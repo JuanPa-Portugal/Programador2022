8b19bdc80a4aa97fd9c464ead62606133422f670
Juan Pablo Portugal <91083049+JuanPa-Portugal@users.noreply.github.com>
2022-08-13 19:26:32 -0300

    Delete ejercicio_4.py

--- ProyectoIntegradorEjerciciosPython/ejercicio_4.py
+++
@@ -1,7 +0,0 @@
-"""
-4) Crear un lista en Python denominado “Perro2” que contenga los   siguientes valores:
-
-      14,  Fido,  12/12/2012 , Macho, 23546987
-
-Recorrer la lista “Perro” con un bucle y mostrar sus elementos por pantalla , comenzando desde el último hasta el primero.
-"""
