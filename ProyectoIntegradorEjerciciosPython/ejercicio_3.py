b3c93dadc4e34a06eaa7004afb45b937154ab6ff
Juan Pablo Portugal <91083049+JuanPa-Portugal@users.noreply.github.com>
2022-08-13 19:27:59 -0300

    Delete ejercicio_3.py

--- ProyectoIntegradorEjerciciosPython/ejercicio_3.py
+++
@@ -1,7 +0,0 @@
-"""
-3)  Crear un lista en Python denominado “Perro” que contenga los siguientes valores:
-
-  13,  Puppy,  12/12/2012 , Macho, 123
-
-Que se corresponde con los datos de un perro de nuestra base de datos (Id_Perro, nombre, fecha de nacimiento, sexo y dni del dueño). Modificar la fecha de nacimiento por 13/12/2012 y reemplazar “dni del dueño” por 28957346 .
-"""
