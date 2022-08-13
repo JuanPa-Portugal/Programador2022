4fc2f9a30b3801ee43f02fce9c236918783b34d1
Juan Pablo Portugal <91083049+JuanPa-Portugal@users.noreply.github.com>
2022-08-13 19:27:39 -0300

    Delete ejercicio_2.py

--- ProyectoIntegradorEjerciciosPython/ejercicio_2.py
+++
@@ -1,5 +0,0 @@
-"""2)   Crear una lista en Python denominada “Dueno2”  y recorrerla con un bucle  mostrando sus elementos por pantalla con excepción del DNI y el apellido. Los elementos de la lista son:
-
-           23546987,  “Maria”,  “Perez”, 4789689,  “Pueyrredon  811”
-
- que representan los datos del dueño de un perro (DNI, nombre, apellido, teléfono y dirección)"""
