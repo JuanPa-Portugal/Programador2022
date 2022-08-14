b4e253009d390c417bbcd7c918bd9f7fd9aa5c6a
Mauro Martinez <mauromartinez10.mtz@gmail.com>
2022-08-13 23:29:22 -0300

    Eliminando ejercicio 4 de la main

--- ProyectoIntegradorEjerciciosPython/ejercicio 4 Python.py
+++
@@ -1,7 +0,0 @@
-"""Creando mi lista Perro2 del tipo heterogénea"""
-
-Perro2 = [14, "Fido",  "12/12/2012", "Macho", 23546987]
-
-"""Recorriendo mi lista y mostrando sus elementos desde el ultimo al primero"""
-for x in range(len(Perro2)-1,-1,-1):
-  print(Perro2[x])
